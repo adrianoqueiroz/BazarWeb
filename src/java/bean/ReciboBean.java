c2991962e464ccf22674efcc251553665c5a7da3
Adriano Queiroz <adriano.nq@gmail.com>
2015-01-30 16:52:02 -0300

    busca por código para visualizar o recibo, mudanças no layout da tela de cadastro de produtos

--- src/java/bean/ReciboBean.java
+++ src/java/bean/ReciboBean.java
@@ -8,8 +8,10 @@ package bean;
 import JPA.VendaJpaController;
 import java.io.Serializable;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import model.Item;
 import model.Venda;
 
@@ -20,8 +22,14 @@ import model.Venda;
 @ManagedBean
 @RequestScoped
 public class ReciboBean implements Serializable {
+
+    @EJB
+    private VendaJpaController vendaJpaController;
+
+    private Integer idProcurado;
+
     private Venda venda;
-    
+
     public Venda getVenda() {
         return venda;
     }
@@ -29,17 +37,44 @@ public class ReciboBean implements Serializable {
     public void setVenda(Venda venda) {
         this.venda = venda;
     }
-    
+
+    public Integer getIdProcurado() {
+        return idProcurado;
+    }
+
+    public void setIdProcurado(Integer idProcurado) {
+        this.idProcurado = idProcurado;
+    }
+
     public float calculaValorCompra() {
         int valor = 0;
         for (Item i : this.getVenda().getItemCollection()) {
             valor += i.getPrecoVenda() * i.getQuantidade();
         }
         return valor;
-    }    
+    }
 
-    public String selecionaRecibo(Venda venda){
+    public String selecionaRecibo(Venda venda) {
         this.venda = venda;
         return "/recibo_venda.xhtml";
+    }
+
+    public String selecionaRecibo() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        try {
+            this.venda = vendaJpaController.findVenda(idProcurado);
+            if (venda.getId() != null) {
+                context.addMessage(null, new FacesMessage("Recibo de venda", venda.getId().toString()));
+                return "/recibo_venda.xhtml";
+            } else {
+                idProcurado = null;
+                context.addMessage(null, new FacesMessage("Falha", "Venda não encontrada!"));
+            }
+
+        } catch (Exception e) {
+        }
+        idProcurado = null;
+        context.addMessage(null, new FacesMessage("Falha", "Venda não encontrada!"));
+        return "/listarvendas.xhtml";
     }
 }
